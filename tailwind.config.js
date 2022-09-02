07a03d20bb48f83ff7fb99d0154766b25b4322f3
serapieTuyishime <tuyiserapie1@gmail.com>
2022-09-02 15:18:17 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      // put what you want to enable but won't overwrite the existing settings
+    }
+  },
+  plugins: [],
+}
